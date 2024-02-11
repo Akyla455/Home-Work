47560d44d918204c1e5bde3fdd2e34cddb26d264
Dmitry <d.diuan455@gmail.com>
2024-02-11 21:58:05 +0300

    first commit

--- app/src/main/java/com/example/homework/RandomGame.kt
+++ app/src/main/java/com/example/homework/RandomGame.kt
@@ -1,2 +1,27 @@
 package com.example.homework
 
+import kotlin.random.Random
+
+fun randomGame(){
+    val min = 1
+    val max = 10
+
+    val randomNumbers = Random.nextInt(min, max +1)
+   while (true){
+       println("Угадайте число от 1 до 10")
+       val userInput = readln().toInt()
+       if (userInput != null && userInput != randomNumbers){
+           if(userInput > randomNumbers){
+               println("Неверно. Загаданное число меньше ")
+           } else println("Неверно. Загаданное число больше")
+       }
+       if(userInput == randomNumbers) {
+           println("Поздравляю! Верный ответ. Сыграем ещё?")
+           break
+       }
+   }
+}
+
+fun main(){
+    randomGame()
+}
