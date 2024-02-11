5c9f5ecd4dd63c5e31145709840a83aa1f695cc3
Dmitry <d.diuan455@gmail.com>
2024-02-11 21:30:29 +0300

    first commit

--- app/src/main/java/com/example/homework/Library.kt
+++ app/src/main/java/com/example/homework/Library.kt
@@ -1,2 +1,66 @@
 package com.example.homework
 
+open class Book(val title: String,
+                val isbn: String,
+                val description: String)
+
+class FictionBook(title: String,
+                  isbn: String,
+                  description: String,
+                  val genre: String):
+                  Book(title, isbn, description)
+
+class ScientificBook(title: String,
+                     isbn: String,
+                     description: String,
+                     val discoveryDate: String):
+                     Book(title, isbn, description)
+
+class ChildrenBook(title: String,
+                   isbn: String,
+                   description: String,
+                   val age: Int):
+                   Book(title, isbn, description)
+
+class Library(){
+    val books = mutableListOf<Book>()
+
+    fun add(book: Book){
+        books.add(book)
+        println("Книга добалена")
+    }
+
+    fun remove(book: Book){
+        books.remove(book)
+        println("Книга удалена")
+    }
+
+    fun search(title: String): Book?{
+        return books.find { it.title == title }
+
+
+
+    }
+
+
+}
+
+fun main(){
+    val library = Library()
+    val childrenBook = ChildrenBook("Колобок", "123","Описание колобка", 5)
+    val fictionBook = FictionBook("Фантастика", "456", "Описание фантастики", "Жанр: детектив")
+    val scientificBook = ScientificBook("Научная книга", "789", "Описание","15.08.1974")
+
+    library.add(childrenBook)
+    library.add(fictionBook)
+    library.add(scientificBook)
+
+
+    val searchLibrary = library.search("Колобок")
+    if(searchLibrary != null){
+        println("Книга найдена ${searchLibrary.title}")
+    } else println("Книга не найдена")
+
+    library.remove(fictionBook)
+}
+
