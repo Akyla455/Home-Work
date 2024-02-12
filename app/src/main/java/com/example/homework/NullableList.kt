f830cdadd59b6a556761055b71f194371969ef2d
Dmitry <d.diuan455@gmail.com>
2024-02-12 16:45:27 +0300

    finish

--- app/src/main/java/com/example/homework/NullableList.kt
+++ app/src/main/java/com/example/homework/NullableList.kt
@@ -1,2 +1,15 @@
 package com.example.homework
 
+fun List<String?>.concatenate(): String {
+    val result = StringBuilder()
+    for(string in this){
+        string?.let{ result.append(it) }
+    }
+    return result.toString()
+}
+
+fun main(){
+    val nullableString = listOf("П", null, "ри", null, null, "ве", null, "т")
+    val concatenateString = nullableString.concatenate()
+    println(concatenateString)
+}
