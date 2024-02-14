d79e90d8a6f4e7bdeff59837866e2e0eeab2b400
Dmitry <d.diuan455@gmail.com>
2024-02-14 22:00:37 +0300

    final

--- app/src/main/java/com/example/homework/RandomGame.kt
+++ app/src/main/java/com/example/homework/RandomGame.kt
@@ -2,42 +2,66 @@ package com.example.homework
 
 import kotlin.random.Random
 
-fun randomGame(){
-    val min = 1
-    val max = 10
-    var playAgain = true
+var playAgain = true
 
-   while (playAgain){
-       val randomNumbers = Random.nextInt(min, max +1)
-       var result = false
-
-       println("Угадайте число от 1 до 10")
-
-       while (!result){
-
-       val userInput = readln().toIntOrNull()
-       if (userInput != null && userInput != randomNumbers){
-           if(userInput > randomNumbers){
-               println("Неверно. Загаданное число меньше ")
-           } else println("Неверно. Загаданное число больше")
-       }
-       if(userInput == randomNumbers) {
-           println("Поздравляю! Верный ответ.")
-           result = true
-       }
-       }
-       println("Сыграем ещё?(да/нет)")
-       val userInput2 = readlnOrNull()?.trim()?.toLowerCase()
-
-       when(userInput2){
-           "да" -> playAgain = true
-           "нет" -> playAgain = false
-           else -> println("Ошибка! Недопустимый ввод. Игра остановлена.")
-       }
-       playAgain = false
-   }
+fun generateRandomNumber(min: Int, max: Int): Int {
+    return (min..max).random()
 }
 
-fun main(){
+fun checkUserInput(userInput: Int, randomNumber: Int): Boolean {
+    if (userInput != randomNumber) {
+        if (userInput > randomNumber) {
+            println("Неверно. Загаданное число меньше ")
+        } else {
+            println("Неверно. Загаданное число больше")
+        }
+        return false
+    } else {
+        println("Поздравляю! Верный ответ.")
+        return true
+    }
+}
+
+fun readUserInput(): Int? {
+    println("Угадайте число от 1 до 10")
+    return readLine()?.toIntOrNull()
+}
+
+fun askToPlayAgain() {
+    println("Сыграем ещё? (да/нет)")
+    val userInput = readLine()?.trim()?.toLowerCase()
+
+    when (userInput) {
+        "да" -> playAgain = true
+        "нет" -> {
+            playAgain = false
+            println("Спасибо за игру.")
+        }
+        else -> {
+            println("Ошибка! Недопустимый ввод.")
+            askToPlayAgain()
+        }
+    }
+}
+
+fun randomGame() {
+    val min = 1
+    val max = 10
+
+    while (playAgain) {
+        val randomNumber = generateRandomNumber(min, max)
+        var result = false
+
+        while (!result) {
+            val userInput = readUserInput()
+            if (userInput != null) {
+                result = checkUserInput(userInput, randomNumber)
+            }
+        }
+        askToPlayAgain()
+    }
+}
+
+fun main() {
     randomGame()
 }
